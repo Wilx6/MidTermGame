63818b26ed7ab7ec2991e5a18707b89d88028cd3
Wilx6 <144147441+Wilx6@users.noreply.github.com>
2023-10-13 18:42:19 -0400

    Added Main Map

--- Midterm/Assets/Script/PlayerMovement.cs
+++ Midterm/Assets/Script/PlayerMovement.cs
@@ -1,16 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 direction;
    
-
-   
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,29 +17,25 @@ public class PlayerMovement : MonoBehaviour
     {
         GetComponent<Rigidbody2D>();
 
-        if (Input.GetKeyDown(KeyCode.A))
-
+        if (Input.GetKeyDown(KeyCode.A)) //Moves to the left
         {
             direction = Vector2.left;
             transform.position = new Vector2(transform.position.x + direction.x, transform.position.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
-
+        if (Input.GetKeyDown(KeyCode.D)) //Moves to the right
         {
             direction = Vector2.right;
             transform.position = new Vector2(transform.position.x + direction.x, transform.position.y);
         }
-
-        if (Input.GetKeyDown(KeyCode.S))
-
+       
+        if (Input.GetKeyDown(KeyCode.S)) //Moves down
         {
             direction = Vector2.down;
             transform.position = new Vector2(transform.position.x, transform.position.y + direction.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
-
+        if (Input.GetKeyDown(KeyCode.W)) //Moves up
         {
             direction = Vector2.up;
             transform.position = new Vector2(transform.position.x, transform.position.y + direction.y);
