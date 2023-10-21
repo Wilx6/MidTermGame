d32f18198fbee298cb372f5fe468a8083934bad1
Wilx6 <144147441+Wilx6@users.noreply.github.com>
2023-10-20 20:55:39 -0400

    Fixed some stuff
    Fixed some text and miss spellings

--- Midterm/Assets/Script/Starting.cs
+++ Midterm/Assets/Script/Starting.cs
@@ -16,7 +16,7 @@ public class Starting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown("space"))
         {
             SceneManager.LoadScene(1);
             
